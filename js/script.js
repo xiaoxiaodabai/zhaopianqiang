aac7a5bfd428ad752f4e74a6feca195bca11d058
boluobaokuangmoshaonv <3310018024@qq.com>
2017-12-03 20:08:13 +0800

    modify

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
+
 $(document).ready(function(){
+            $('#picture_list').mousewheel(function(event, delta) {
+                this.scrollLeft -= (delta * 60);
+                event.preventDefault();
+      
+            })
             $('img').fadeIn(2000).slideDown();
             $('img').hover(function(){
                 $(this).next('span').css({visibility: "visible"});
@@ -6,6 +12,8 @@ $(document).ready(function(){
             $('img').mouseout(function(){
                 $(this).next('span').css({visibility: "hidden"});
             })
+
+            
             function ShowDiv(show_div,bg_div){
                 document.getElementById(show_div).style.display='block';
                 document.getElementById(bg_div).style.display='block' ;
@@ -16,7 +24,9 @@ $(document).ready(function(){
                         _popupWeight = $("#"+show_div).width();//获取弹出层宽度
                 _posiTop = (_windowHeight - _popupHeight)/2;
                 _posiLeft = (_windowWidth - _popupWeight)/2;
-                $("#"+show_div).css({"left": _posiLeft + "px","top":_posiTop + "px","display":"block"});//设置position
+                $("#"+show_div).css({"left": _posiLeft + "px","top":_posiTop + "px","display":"flex"});//设置position
+                
+            
             };
             //关闭弹出层
             function CloseDiv(show_div,bg_div)
@@ -28,9 +38,42 @@ $(document).ready(function(){
             $(".btn").click(function () {
                 var src = $(this).attr("src");
                 $("#showcont").attr("src",src);
-                ShowDiv('MyDiv','fade')
+                ShowDiv('MyDiv','fade');
+                
             });
+
             $("#fade").click(function () {
                 CloseDiv('MyDiv','fade')
             });
+            
+             $(".two").click(function(){
+                 var img_urls = [];
+                 $(".btn").each(function(){
+                     img_urls.push($(this).attr("src"));
+                 })
+                var src= $(".show").attr("src");
+                for(var i=0;i<img_urls.length;i++)
+                {
+                    if(img_urls[i]==src)
+                    {
+                        $("#showcont").attr("src",img_urls[i+1]);
+                        ShowDiv('MyDiv','fade');
+                    }
+                }
             })
+            $(".one").click(function(){
+                 var img_urls = [];
+                 $(".btn").each(function(){
+                     img_urls.push($(this).attr("src"));
+                 })
+                var src= $(".show").attr("src");
+                for(var i=0;i<img_urls.length;i++)
+                {
+                    if(img_urls[i]==src)
+                    {
+                        $("#showcont").attr("src",img_urls[i-1]);
+                        ShowDiv('MyDiv','fade');
+                    }
+                }
+            })
+})
